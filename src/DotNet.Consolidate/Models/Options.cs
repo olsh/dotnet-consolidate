354b80907ef53ddc8b3ba0c2016765de4b94f6fe
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-11-09 20:37:59 +0100

    Fix order of Options properties to make options parsing work again

--- src/DotNet.Consolidate/Models/Options.cs
+++ src/DotNet.Consolidate/Models/Options.cs
@@ -6,15 +6,18 @@ namespace DotNet.Consolidate.Models
 {
     public class Options
     {
-        public Options(ICollection<string>? solutions, ICollection<string>? packageIds, ICollection<string>? excludedPackageIds, string? excludedPackageVersionsRegex, bool readDirectoryBuildProps, bool reportOverridenDirectoryBuildProps)
+        public Options(ICollection<string>? solutions, ICollection<string>? packageIds, ICollection<string>? excludedPackageIds, string excludedPackageVersionsRegex, bool readDirectoryBuildProps, bool reportOverridenDirectoryBuildProps)
         {
+            Solutions = solutions;
             PackageIds = packageIds;
             ExcludedPackageIds = excludedPackageIds;
             ExcludedPackageVersionsRegex = excludedPackageVersionsRegex;
-            Solutions = solutions;
             ReadDirectoryBuildProps = readDirectoryBuildProps;
             ReportOverridenDirectoryBuildProps = reportOverridenDirectoryBuildProps;
         }
+
+        [Option('s', "solutions", Required = false, HelpText = "Target solutions for checking. If not specified, all solutions in the working directory will be analyzed.")]
+        public ICollection<string>? Solutions { get; }
 
         [Option('p', "packageIds", Required = false, HelpText = "Package IDs for checking.")]
         public ICollection<string>? PackageIds { get; }
@@ -23,10 +26,7 @@ namespace DotNet.Consolidate.Models
         public ICollection<string>? ExcludedPackageIds { get; }
 
         [Option('v', "excludedVersionsRegex", Required = false, HelpText = "A regular expression to match package versions that will be skipped during checking.")]
-        public string? ExcludedPackageVersionsRegex { get; }
-
-        [Option('s', "solutions", Required = false, HelpText = "Target solutions for checking. If not specified, all solutions in the working directory will be analyzed.")]
-        public ICollection<string>? Solutions { get; }
+        public string ExcludedPackageVersionsRegex { get; }
 
         [Option('d', "directoryBuildProps", Required = false, Default = true, HelpText = "Take Directory.Build.props files into account")]
         public bool ReadDirectoryBuildProps { get; }
